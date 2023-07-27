0009a36793a0d47cced22bb475f57d0b44db56f8
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2023-07-27 11:58:23 +0530

    Changed to only show optimal case
    m=capacity
    n=no of items
    p=profit array
    w=weight array

--- GreedyKnapsack.java
+++ GreedyKnapsack.java
@@ -14,113 +14,39 @@ public class GreedyKnapsack {
 			X[i]=0;
 			index[i]=i;
 		}
-		switch(sol)
+		for(int i=0;i<n;i++)
 		{
-			case 0://Decreasing order of profit
-				for(int i=0;i<n;i++)
+			for(int j=1;j<n-i;j++)
+			{
+				if(R[j-1]>R[j])
 				{
-					for(int j=1;j<n-i;j++)
-					{
-						if(P[j-1]>P[j])
-						{
-							temp=P[j];
-							P[j]=P[j-1];
-							P[j-1]=temp;
-							temp=index[j];
-							index[j]=index[j-1];
-							index[j-1]=(int)temp;
-						}
-					}
+					temp=R[j];
+					R[j]=R[j-1];
+					R[j-1]=temp;
+					temp=index[j];
+					index[j]=index[j-1];
+					index[j-1]=(int)temp;
 				}
-				ind=n-1;
-				while(m>0&&ind>=0)
-				{
-					temp=W[index[ind]];
-					if(temp<=m)
-					{
-						m-=temp;
-						X[index[ind]]=1;
-					}
-					else
-					{
-						X[index[ind]]=m/temp;
-						m=0;
-					}
-					profit+=P[ind]*X[index[ind]];
-//					System.out.println(m+"\t"+profit);
-					ind--;
-				}
-				break;
-			case 1://Increasing order of weight
-				for(int i=0;i<n;i++)
-				{
-					for(int j=1;j<n-i;j++)
-					{
-						if(W[j-1]>W[j])
-						{
-							temp=W[j];
-							W[j]=W[j-1];
-							W[j-1]=temp;
-							temp=index[j];
-							index[j]=index[j-1];
-							index[j-1]=(int)temp;
-						}
-					}
-				}
-				ind=0;
-				while(m>0&&ind<n)
-				{
-					temp=W[ind];
-					if(temp<=m)
-					{
-						m-=temp;
-						X[index[ind]]=1;
-					}
-					else
-					{
-						X[index[ind]]=m/temp;
-						m=0;
-					}
-					profit+=P[index[ind]]*X[index[ind]];
-//					System.out.println(profit);
-					ind++;
-				}
-				break;
-			case 2://Optimal solution
-				for(int i=0;i<n;i++)
-				{
-					for(int j=1;j<n-i;j++)
-					{
-						if(R[j-1]>R[j])
-						{
-							temp=R[j];
-							R[j]=R[j-1];
-							R[j-1]=temp;
-							temp=index[j];
-							index[j]=index[j-1];
-							index[j-1]=(int)temp;
-						}
-					}
-				}
-				ind=n-1;
-				while(m>0&&ind>=0)
-				{
-					temp=W[index[ind]];
-					if(temp<=m)
-					{
-						m-=temp;
-						X[index[ind]]=1;
-					}
-					else
-					{
-						X[index[ind]]=m/temp;
-						m=0;
-					}
-					profit+=P[index[ind]]*X[index[ind]];
-//					System.out.println(profit);
-					ind--;
-				}
+			}
 		}
+		ind=n-1;
+		while(m>0&&ind>=0)
+		{
+			temp=W[index[ind]];
+			if(temp<=m)
+			{
+				m-=temp;
+				X[index[ind]]=1;
+			}
+			else
+			{
+				X[index[ind]]=m/temp;
+				m=0;
+			}
+			profit+=P[index[ind]]*X[index[ind]];
+			ind--;
+		}
+		System.out.printf("profit:%.3f\n",profit);
 		return X;
 	}
 }
